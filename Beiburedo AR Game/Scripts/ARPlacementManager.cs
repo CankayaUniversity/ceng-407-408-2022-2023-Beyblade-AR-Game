099ee21fbcef7016f93bc179e20e89a0c787923d
Erce Altun <57298922+ercealtun@users.noreply.github.com>
2023-04-15 11:11:26 +0300

    First AR implementation done
    AR placement and raycasting mechanicsm are done

---
+++ Beiburedo AR Game/Scripts/ARPlacementManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacementManager : MonoBehaviour
+{
+    private ARRaycastManager m_ARRaycastManager;
+    private static List<ARRaycastHit> raycast_Hits = new List<ARRaycastHit>();
+
+    public Camera aRCamera;
+
+    public GameObject battleArenaGameobject;
+    private void Awake()
+    {
+        m_ARRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 centerOfScreen = new Vector3(Screen.width / 2, Screen.height / 2);
+        Ray ray = aRCamera.ScreenPointToRay(centerOfScreen);
+
+        if (m_ARRaycastManager.Raycast(ray, raycast_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Intersection
+            Pose hitPose = raycast_Hits[0].pose;
+
+            Vector3 positionToBePlaced = hitPose.position;
+
+            battleArenaGameobject.transform.position = positionToBePlaced;
+        }
+    }
+}
